86572391cdc3da4e4799984ec6e7c69f83dbda6a
GJ713932 <116373991+GJ713932@users.noreply.github.com>
2023-03-27 16:49:26 -0700

    ummm

--- Unit1-PlayerControlJonasson/Assets/Scripts/PlayerController.cs
+++ Unit1-PlayerControlJonasson/Assets/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
+
 
         //THis is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
@@ -27,13 +27,13 @@ public class PlayerController : MonoBehaviour
         // Moves the car forward based on horizontal input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotate the car based on horizontal input 
-=======
+
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
->>>>>>> c47deff28818a437eca9d2641ae7519876dae89d
+
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
